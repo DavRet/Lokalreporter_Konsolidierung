1b261afbefbbfc993024480445d0bec8c7bd9c58
DavRet <david.r@appmeisterei.de>
2016-08-23 18:45:35 +0200

    Neue Anordnung List Items

--- src/lokalreporterView.js
+++ src/lokalreporterView.js
@@ -193,7 +193,7 @@ NewsMap.lokalreporterView = (function () {
                 if (data['items'][i]['geoData'].length) {
                     artikelOrt = data['items'][i]['geoData'][0]['name'];
                 }
-                EIDI = data['items'][i]['id']
+                EIDI = data['items'][i]['id'];
                 artikelTitel = data['items'][i]['title'];
                 artikelLink = data['items'][i]['originalLink'];
                 pubDate = data['items'][i]['date'];
@@ -291,7 +291,8 @@ NewsMap.lokalreporterView = (function () {
                 }
             });
 
-            $(".article-list").css('min-height', hi);},
+            $(".article-list").css('min-height', hi);
+        },
 
         truncateOnWord = function (str, limit) {
             var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
